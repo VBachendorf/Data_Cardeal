c2ceb9c573f80ba6bb8f10d553ab9734d57d1991
Vanderlei Bachendorf <119347746+VBachendorf@users.noreply.github.com>
2025-03-06 20:29:31 -0300

    atividade2

---
+++ 2025-03-06/obtendo_nomes_e_valores.py
@@ -0,0 +1,10 @@
+nome_cliente = str(input('Digite o nome do Cliente: '))
+total_compra = float(input('Total da Compra: '))
+valor_pago = float(input('Total Pagamento do Cliente: '))
+
+#   O Cliente João efetuou uma compra de 455.00 R$
+#   João realizou o pagamento de 470.00 R$
+#   João receberá de troco 15.00 R$
+#
+print('O Cliente ',nome_cliente,' efetuou uma compra de ',total_compra,'R$')
+print(f'O Cliente {nome_cliente} efetuou uma compra de {total_compra} R$') 
