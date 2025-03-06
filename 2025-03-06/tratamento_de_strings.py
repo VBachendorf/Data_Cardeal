770d93108db23ce303d49b8ca06c54eed495f34b
Vanderlei Bachendorf <119347746+VBachendorf@users.noreply.github.com>
2025-03-06 19:27:24 -0300

    commit code 1

---
+++ 2025-03-06/tratamento_de_strings.py
@@ -0,0 +1,11 @@
+print('Olá alunos(as) do curso de ciencia de dados')
+nome = 'Josão pé de tomate'
+
+mensagem_boas_vindas = ' Olá '
+print(mensagem_boas_vindas,nome)
+
+#print formatado
+print(f'Olá \n{nome}')
+
+# \n <- quebra a linha do texto
+
