004a2e6dcae8cde548c603176728b455fdfa0ae8
Vanderlei Bachendorf <119347746+VBachendorf@users.noreply.github.com>
2025-03-06 20:47:02 -0300

    jasdiaos

--- 2025-03-06/obtendo_nomes_e_valores.py
+++ 2025-03-06/obtendo_nomes_e_valores.py
@@ -5,6 +5,8 @@ valor_pago = float(input('Total Pagamento do Cliente: '))
 #   O Cliente João efetuou uma compra de 455.00 R$
 #   João realizou o pagamento de 470.00 R$
 #   João receberá de troco 15.00 R$
-#
+
+#o primeiro print é uma concatenação de strings 
 print('O Cliente ',nome_cliente,' efetuou uma compra de ',total_compra,'R$')
+# o segundo print é um texto único
 print(f'O Cliente {nome_cliente} efetuou uma compra de {total_compra} R$') 
