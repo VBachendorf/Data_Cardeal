ad3e883d9f5b5c408bfa956fe40a08cfb8264b49
Vanderlei Bachendorf <119347746+VBachendorf@users.noreply.github.com>
2025-05-06 00:51:21 -0300

    aqwew

---
+++ 2025-05-05/if_num_dia.py
@@ -0,0 +1,16 @@
+dia=int(4)
+#com base no dia ele retornará o nome do dia da semana
+if dia == 1:
+    print('Segunda Feira')
+elif dia == 2:
+    print('Terça Feira')
+elif dia == 3:
+    print('Quarta Feira')
+elif dia == 4:
+    print('Quinta Feira')
+elif dia == 5:
+    print('Sexta Feira')
+elif dia == 6:
+    print('Sábado')
+elif dia == 7:
+    print('Domingo')
--- 2025-05-05/operador_math.py
+++ 2025-05-05/operador_math.py
@@ -1,6 +1,12 @@
-# este código irá retornar o dia da semana
-dia = 1
-# importa um dia em número e retornará em nome de dia
-match dia:
-    case 1:
-        print('Segunda Feira')
+def dia_semana(dia):
+    # importa um dia em número e retornará em nome de dia
+    match dia:
+        case 1:
+            print('Segunda Feira')
+        case 2:
+            print('Terça Feira')
+        case 3:
+            print('QUARTA')
+        case 4:
+            print('QUINTA Feira')
+dia_semana(2)
